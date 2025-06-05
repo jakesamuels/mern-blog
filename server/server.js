718bed501cef30b72efcf784cb08bf057a5cd1e7
Jake Samuels <samuels242@googlemail.com>
2025-06-05 14:14:05 +0100

    Connect to DB

--- server/server.js
+++ server/server.js
@@ -1,6 +1,17 @@
 import "dotenv/config";
 import app from "./app.js";
+import { connectDB } from "./config/db.js";
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+  } catch (error) {
+    console.error("Failed to connect to database: ", error);
+    process.exit(1);
+  }
+};
+
+startServer();
