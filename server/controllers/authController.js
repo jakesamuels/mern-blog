9b2ca2b05080268a067df45a74b316b5aab814a7
Jake Samuels <samuels242@googlemail.com>
2025-06-05 15:08:21 +0100

    use catchASync

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,16 +1,13 @@
 import User from "../models/User.js";
+import catchAsync from "./../utils/catchAsync.js";
 
-export const registerUser = async (req, res, next) => {
-  try {
-    const newUser = await User.create(req.body);
+export const registerUser = catchAsync(async (req, res, next) => {
+  const newUser = await User.create(req.body);
 
-    res.status(201).json({
-      status: "success",
-      data: {
-        user: newUser,
-      },
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
+  res.status(201).json({
+    status: "success",
+    data: {
+      user: newUser,
+    },
+  });
+});
