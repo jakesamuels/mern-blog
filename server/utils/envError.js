9910f69e9e55a13ce0b296a420dc3aedc552e899
Jake Samuels <samuels242@googlemail.com>
2025-06-05 15:02:19 +0100

    Handle environment errors

---
+++ server/utils/envError.js
@@ -0,0 +1,26 @@
+// Error Handler for Development Environment
+const devError = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+// Error Handler for Production Environment
+const prodError = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    res.status(500).json({
+      status: "error",
+      message: "something went very wrong!",
+    });
+  }
+};
+
+export { devError, prodError };
