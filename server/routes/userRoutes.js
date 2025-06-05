769d13f622188bb1bff212ab21d3db9792004c65
Jake Samuels <samuels242@googlemail.com>
2025-06-05 21:47:12 +0100

    GET all users

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { registerUser } from "./../controllers/authController.js";
+import { registerUser, login } from "./../controllers/authController.js";
+import { getAllUsers } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", login);
+
+router.get("/", getAllUsers);
 
 export default router;
