b0e8b66a0496ccaf03bfea8be00bc73d82194db8
Jake Samuels <samuels242@googlemail.com>
2025-06-05 14:15:14 +0100

    Hash password

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,6 @@
 import { mongoose, Schema } from "mongoose";
+import validator from "validator";
+import bcrypt from "bcryptjs";
 
 const UserSchema = new Schema(
   {
@@ -6,19 +8,31 @@ const UserSchema = new Schema(
       type: String,
       required: [true, "Username is required"],
       unique: true,
+      trim: true,
     },
     email: {
       type: String,
       required: [true, "Email is required"],
       unique: true,
+      trim: true,
+      lowercase: true,
+      validate: [validator.isEmail, "Please provide a valid email"],
     },
     password: {
       type: String,
       required: [true, "Password is required"],
+      minlength: 8,
     },
     passwordConfirm: {
       type: String,
       required: [true, "Please confirm your password"],
+      validate: {
+        // This only works on CREATE and SAVE!
+        validator: function (el) {
+          return el === this.password;
+        },
+        message: "Passwords do not match",
+      },
     },
     createdAt: {
       type: Date,
@@ -31,6 +45,20 @@ const UserSchema = new Schema(
   },
   { timestamps: true }
 );
+
+UserSchema.pre("save", async function (next) {
+  // Only run this function is password was actual modified
+  if (!this.isModified("password")) return next();
+
+  // Hash the password with cost of 12
+  const salt = await bcrypt.genSalt(12);
+  const hash = await bcrypt.hash(this.password, salt);
+  this.password = hash;
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
 
 const User = mongoose.model("User", UserSchema);
 
