8c45ea5fd1413c4b9b44d9be7e0dba44bfe7751f
Jake Samuels <samuels242@googlemail.com>
2025-06-05 14:14:48 +0100

    Config user route and import router

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 import express from "express";
 import cors from "cors";
 
+import userRouter from "./routes/userRoutes.js";
+
 const app = express();
 
+// Middleware
 app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => res.send("Server running"));
+app.use("/api/users", userRouter);
 
 export default app;
