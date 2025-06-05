e8160b0674ca6a6275533072a066d2607207cb07
Jake Samuels <samuels242@googlemail.com>
2025-06-05 20:48:38 +0100

    Handle rejection and uncaught exceptions

--- server/server.js
+++ server/server.js
@@ -1,13 +1,32 @@
 import "dotenv/config";
+
+process.on("uncaughtException", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNHANDLED EXCEPTION! 💥 Shutting down...");
+  process.exit(1);
+});
+
 import app from "./app.js";
 import { connectDB } from "./config/db.js";
 
 const PORT = process.env.PORT || 8000;
 
+let server;
+
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+    server = app.listen(PORT, () =>
+      console.log(`Server listening on port ${PORT}`)
+    );
   } catch (error) {
     console.error("Failed to connect to database: ", error);
     process.exit(1);
