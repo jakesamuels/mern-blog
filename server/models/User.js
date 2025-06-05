7051116deee467815a552c15af0e45725d12587e
Jake Samuels <samuels242@googlemail.com>
2025-06-05 12:47:05 +0100

    User model

---
+++ server/models/User.js
@@ -0,0 +1,37 @@
+import { mongoose, Schema } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    passwordConfirm: {
+      type: String,
+      required: [true, "Please confirm your password"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
