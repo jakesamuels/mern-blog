599065f8f60184c654eb924db57af623670aee4f
Jake Samuels <samuels242@googlemail.com>
2025-06-05 21:46:56 +0100

    getAllUsers

---
+++ server/controllers/userController.js
@@ -0,0 +1,14 @@
+import User from "../models/User.js";
+import catchAsync from "../utils/catchAsync.js";
+
+export const getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: "success",
+    results: users.length,
+    data: {
+      users,
+    },
+  });
+});
