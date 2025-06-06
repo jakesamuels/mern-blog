f8142b0c2b517fdc1f0b341651060edc63a18984
Jake Samuels <samuels242@googlemail.com>
2025-06-06 19:38:57 +0100

    deleteComment route

--- server/routes/commentRoutes.js
+++ server/routes/commentRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { addComment } from "./../controllers/commentController.js";
+import {
+  addComment,
+  deleteComment,
+} from "./../controllers/commentController.js";
 import { protect } from "./../controllers/authController.js";
 
 const router = express.Router();
 
-router.route("/:postId/comments").post(protect, addComment);
+router.route("/:postId/comment").post(protect, addComment);
+
+router.route("/:postId/comment/:commentId").delete(protect, deleteComment);
 
 export default router;
