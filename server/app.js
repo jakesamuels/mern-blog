cd5d3fa705e9273295d822328a24a960bc410a14
Jake Samuels <samuels242@googlemail.com>
2025-06-06 17:00:59 +0100

    Import and use commentRouter

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ import AppError from "./utils/appError.js";
 import globalErrorHandler from "./controllers/errorController.js";
 import userRouter from "./routes/userRoutes.js";
 import postRouter from "./routes/postRoutes.js";
+import commentRouter from "./routes/commentRoutes.js";
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(express.json());
 // Routes
 app.use("/api/users", userRouter);
 app.use("/api/posts", postRouter);
+app.use("/api/comments", commentRouter);
 
 app.all("/{*splat}", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server.`, 400));
