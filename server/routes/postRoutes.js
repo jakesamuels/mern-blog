cab77aa518309cade1c95b864a21a9f5b001b5bf
Jake Samuels <samuels242@googlemail.com>
2025-06-06 15:05:20 +0100

    deletePost route

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -4,12 +4,17 @@ import {
   getPost,
   createPost,
   updatePost,
+  deletePost,
 } from "./../controllers/postController.js";
 import { protect } from "./../controllers/authController.js";
 
 const router = express.Router();
 
 router.route("/").get(getAllPosts).post(protect, createPost);
-router.route("/:id").get(getPost).put(protect, updatePost);
+router
+  .route("/:id")
+  .get(getPost)
+  .put(protect, updatePost)
+  .delete(protect, deletePost);
 
 export default router;
