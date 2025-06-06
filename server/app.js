fe2f9886a949c330d1f0af1c7b2ad8b71d7f6efe
Jake Samuels <samuels242@googlemail.com>
2025-06-07 00:10:22 +0100

    Remove sanitizer

--- server/app.js
+++ server/app.js
@@ -17,17 +17,16 @@ app.use(cors());
 // Set security HTTP headers
 app.use(helmet());
 
+// Body parser, reading data from body into req.body
+app.use(express.json());
+
 // Limit requests from same IP
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: "Too many requests from this IP, please try again in an hour!",
 });
-
 app.use("/api", limiter);
-
-// Body parser, reading data from body into req.body
-app.use(express.json());
 
 // Routes
 app.use("/api/users", userRouter);
