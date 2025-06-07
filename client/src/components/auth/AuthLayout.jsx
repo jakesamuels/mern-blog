18beb8d66a92bb8a40b5a76d7063c6460cbb26da
Jake Samuels <samuels242@googlemail.com>
2025-06-07 13:47:09 +0100

    AuthLayout

--- client/src/components/auth/AuthLayout.jsx
+++ client/src/components/auth/AuthLayout.jsx
@@ -1,5 +1,17 @@
+import { Outlet, useLocation } from "react-router";
+
 const AuthLayout = () => {
-  return <div>AuthLayout</div>;
+  const location = useLocation();
+  const isRegisterPage = location.pathname === "/register";
+  const isLoginPage = location.pathname === "/login";
+
+  return (
+    <div>
+      <h1>{isRegisterPage ? "Join Us Today!" : "Welcome Back!"}</h1>
+
+      <Outlet />
+    </div>
+  );
 };
 
 export default AuthLayout;
