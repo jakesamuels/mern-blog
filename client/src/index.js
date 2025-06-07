1d449eb1c7d2b55617fedc4dfe5b65cddefff7b0
Jake Samuels <samuels242@googlemail.com>
2025-06-07 14:02:52 +0100

    Config auth routes

--- client/src/index.js
+++ client/src/index.js
@@ -3,26 +3,32 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router";
+import AuthProvider from "./context/AuthContext";
 
+// Auth Components
 import AuthLayout from "./components/auth/AuthLayout";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route index element={<App />} />
+      <AuthProvider>
+        <Routes>
+          <Route index element={<App />} />
 
-        {/* <Route element={<AuthLayout />}>
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-        </Route> */}
+          <Route element={<AuthLayout />}>
+            <Route path="register" element={<Register />} />
+            <Route path="login" element={<Login />} />
+          </Route>
 
-        {/* <Route path="dashboard" element={<Dashboard />}>
+          {/* <Route path="dashboard" element={<Dashboard />}>
           <Route index element={<Home />} />
           <Route path="settings" element={<Settings />} />
         </Route> */}
-      </Routes>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
