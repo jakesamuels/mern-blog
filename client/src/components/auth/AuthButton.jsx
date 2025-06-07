37549c956d6187b3764225ccf76099cf0425c43d
Jake Samuels <samuels242@googlemail.com>
2025-06-07 21:03:46 +0100

    AuthButton

--- client/src/components/auth/AuthButton.jsx
+++ client/src/components/auth/AuthButton.jsx
@@ -1,5 +1,9 @@
 const AuthButton = ({ children }) => {
-  return <button type="submit">{children}</button>;
+  return (
+    <button type="submit" className="auth-btn">
+      {children}
+    </button>
+  );
 };
 
 export default AuthButton;
