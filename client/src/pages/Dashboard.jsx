54ddb1cb4ad8979a43712a5bcc82b04442ba05fb
Jake Samuels <samuels242@googlemail.com>
2025-06-08 15:04:21 +0100

    Add Post link and Outlet

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import { NavLink, Outlet } from "react-router";
 const Dashboard = () => {
   return (
     <div className="dashboard-page">
+      <h1>Dashboard</h1>
       <nav>
         <NavLink to="/dashboard/addPost">Add Post</NavLink>
       </nav>
